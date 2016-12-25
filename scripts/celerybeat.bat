36af9c631333d9af21cf0f86671b32ebc7009904
Rakesh Mukundan <rakesh@rakeshmukundan.in>
2016-12-25 20:54:24 +0100

    Fixed issue with different sqlite filenames for celeryd and celerybeat in windows

--- scripts/celerybeat.bat
+++ scripts/celerybeat.bat
@@ -1,1 +1,1 @@
-C:\spotipo\env\Scripts\celery -A celeryworker.celery  --workdir=C:\\spotipo\\ beat -b "sqla+sqlite:///instance\celeryd.db" --loglevel=DEBUG -f logs\\celerybeat.log
+C:\spotipo\env\Scripts\celery -A celeryworker.celery  --workdir=C:\\spotipo\\ beat -b "sqla+sqlite:///instance\celerydb.db" --loglevel=DEBUG -f logs\\celerybeat.log
